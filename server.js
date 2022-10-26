91158e6a6f0d409fcce9447d23db61bf6d5b4ee8
William James <Williamj3795@gmail.com>
2022-10-26 17:31:36 -0400

    const variables for modules/path/ and fs

---
+++ server.js
@@ -0,0 +1,6 @@
+const Manager = require("./modules/Manager");
+const Engineer = require("./modules/Engineer");
+const Intern = require("./modules/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
