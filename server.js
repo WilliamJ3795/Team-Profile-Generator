d28336239f299fbd787f8d59aca7eb98551fdf06
William James <Williamj3795@gmail.com>
2022-10-28 12:42:55 -0400

    required tech and imported modules

--- server.js
+++ server.js
@@ -1,153 +1,38 @@
-const Manager = require("./modules/Manager");
-const Engineer = require("./modules/Engineer");
-const Intern = require("./modules/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
+const jest = require("jest");
 const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+let pdf = require("html-pdf");
 
-const TEAMPROFILES_DIR = path.resolve(__dirname, "teamprofiles");
-const teamprofilesPath = path.join(TEAMPROFILES_DIR, "startup.html");
+let options = {
+  format: "Letter"
+};
 
-const render = require("./modules/htmlGenerate.js");
+//Module Imports
+const generateHTML = require("output/generateHTML.js");
+const Employee = require("./lib/Employee.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
+//==================================
+// sets up express app
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Code to use inquirer to gather information about the  team members,
-// and to create objects for each team member.
-const teamMembers = [];
+//allows express app to handle data parsing
+app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+//===================================
 
-function start() {
-  managerInq();
-}
 
-function managerInq() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is the name of the team manager?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Team Manager's ID number:",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Team Manager's email address:",
-        },
-        {
-          type: "input",
-          name: "officeNumber",
-          message: "Team Manager's office number:",
-        },
-      ])
-      .then((val) => {
-        const manager = new Manager(
-          val.name,
-          val.id,
-          val.email,
-          val.officeNumber
-        );
-        console.table(manager);
-        teamMembers.push(manager);
-        addTeamMember();
-      });
-  }
-
-  function addTeamMember() {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "what_type",
-          message: "Add an engineer or intern to the team?",
-          choices: ["Engineer", "Intern", "Not at this time"],
-        },
-      ])
-      .then((val) => {
-        if (val.what_type === "Engineer") {
-          engineerInq();
-        } else if (val.what_type === "Intern") {
-          internInq();
-        } else {
-          createFile();
-        }
-      });
-  }
-  function engineerInq() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "Engineer's name?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Engineer's ID number:",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Engineer's email address:",
-        },
-        {
-          type: "input",
-          name: "github",
-          message: "What is the Engineer's GitHub Username?",
-        },
-      ])
-      .then((val) => {
-        const engineer = new Engineer(val.name, val.id, val.email, val.github);
-        console.table(engineer);
-        teamMembers.push(engineer);
-        addTeamMember();
-      });
-  }
-  function internInq() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "Intern's name?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Intern's ID number:",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Intern's email address:",
-        },
-        {
-          type: "input",
-          name: "school",
-          message: "What school does/did the intern attend?",
-        },
-      ])
-      .then((val) => {
-        const intern = new Intern(val.name, val.id, val.email, val.school);
-        console.table(intern)
-        teamMembers.push(intern);
-        addTeamMember();
-      });
-  }
-
-  function createFile() {
-    if (!fs.existsSync(TEAMPROFILES_DIR)) {
-      fs.mkdirSync(TEAMPROFILES_DIR);
-    } else {
-  
-      fs.writeFileSync(teamprofilesPath, render(teamMembers), "UTF-8");
-      console.log("File created in the teamprofiles folder");
-    }
-    
-  }
-  
-  start();
-  
+//Employees DATA
+const employees = [];
+const engineers = [];
+const interns = [];
+const managers = [];
+let id = 0;
+let response;
